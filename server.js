60902c1194299a9665565f28fd6183e74a66f9d5
LenilsonLima <lenilson.pantoja@estudante.ifms.edu.br>
2025-11-27 15:25:13 -0400

    1

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const app = require('./app.js');
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5002;
 
 app.listen(PORT, () => {
   console.log(`Servidor Express está ouvindo na porta ${PORT}`);
